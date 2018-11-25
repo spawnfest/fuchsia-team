19c81bb9b0120db9adb6597e83152167813db6eb
Stefano Ordine <stefano.ordine@gmail.com>
2018-11-25 13:24:12 +0100

    First win! we have stat calculated data into the browser!

--- lib/eligit_web/controllers/page_controller.ex
+++ lib/eligit_web/controllers/page_controller.ex
@@ -1,4 +1,5 @@
 require IEx
+require Gitstat
 
 defmodule EligitWeb.PageController do
   use EligitWeb, :controller
@@ -8,6 +9,20 @@ defmodule EligitWeb.PageController do
   end
 
   def create(conn, %{ "repo" => %{ "url" => url }} = params) do
-    render(conn, "report.html", url: url)
+    # First win!!! TODO: refactor all this shit! =D
+    repo_local_path = "/tmp/eligit"
+    File.rm_rf!(repo_local_path)
+    File.mkdir!(repo_local_path)
+    { clone_status, repo } = Git.clone [url, repo_local_path]
+
+    { :ok, pid } = Gitstat.Fame.start_link()
+    fame = Gitstat.Fame.run(pid, repo_local_path) |> Enum.reject &is_nil/1
+
+    render(conn, "report.html",
+      url: url,
+      clone_status: clone_status,
+      repo: repo,
+      fame: fame
+    )
   end
 end
--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Eligit.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.0"}
+      {:credo, "~> 1.0"},
+      {:git_cli, "~> 0.2"},
     ]
   end
 
