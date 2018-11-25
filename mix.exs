c8d0ec455a301e73be0b77a4debb04d097fc943a
Fabrizio Monti <fabrizio.monti@welaika.com>
2018-11-25 11:35:16 +0100

    Add my credo

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Eligit.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.0"}
     ]
   end
 
