c19dc599cc8bd438345ad16d29dad0e2d2de2238
Matteo Piotto <matteo.piotto@welaika.com>
2018-11-25 21:30:36 +0100

    Microfix prod settings

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :eligit, EligitWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "elig.it", port: System.get_env("PORT") || 4000],
+  url: [host: "elig.it", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
